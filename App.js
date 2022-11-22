8018ac900add0cdbe28e2f470b17e19f160d13b0
karthiknemmani <karthiknemmani@gmail.com>
2022-11-22 16:16:42 -0600

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import React from "react";
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  SafeAreaView,
+  Touchable,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Welcome to MealSync</Text>
-      <View>
-      <Image source={require("./assets/icon.png")} />
-      </View>
+      <Image
+        style={{ width: "50%", height: "25%" }}
+        source={require("./assets/AppLogo.png")}
+      />
+      <TextInput style={styles.inputText} placeholder="Username" />
+      <TextInput style={styles.inputText} placeholder="Password" />
+      <Button title="Sign in" color="#E3967E" />
+      <TouchableOpacity>
+        <Text>Forgot password?</Text>
+      </TouchableOpacity>
+      <Button title="New? Sign up" color="#E3967E" />
+      <Text>or continue with</Text>
+      <Image
+        style={{ width: "10%", height: "10%" }}
+        source={require("./assets/google.png")}
+      />
+      <Image
+        style={{ width: "10%", height: "10%" }}
+        source={require("./assets/facebook.png")}
+      />
     </SafeAreaView>
   );
 }
@@ -15,8 +42,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
+  },
+  inputText: {
+    height: 20,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
