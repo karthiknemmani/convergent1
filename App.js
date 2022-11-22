dcac3f309089092d47a3354d058b1333074a8862
karthiknemmani <karthiknemmani@gmail.com>
2022-11-22 17:15:36 -0600

    test

--- App.js
+++ App.js
@@ -27,29 +27,38 @@ export default function App() {
       </TouchableOpacity>
       <Button title="New? Sign up" color="#E3967E" />
       <Text>or continue with</Text>
-      <Image
-        style={{ width: "10%", height: "10%" }}
-        source={require("./assets/google.png")}
-      />
-      <Image
-        style={{ width: "10%", height: "10%" }}
-        source={require("./assets/facebook.png")}
-      />
+      {/* <SafeAreaView style={styles.iconView}>
+        <Image
+          style={{ width: "10%", height: "10%" }}
+          source={require("./assets/google.png")}
+        />
+        <Image
+          style={{ width: "10%", height: "10%" }}
+          source={require("./assets/facebook.png")}
+        />
+      </SafeAreaView> */}
     </SafeAreaView>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputText: {
-    height: 20,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    inputText: {
+      height: 20,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+    iconView: {
+      flexDirection: "row",
+    },
+    googlefacebook: {
+      width: "20%",
+      height: "20%",
+    },
+  });
+}
