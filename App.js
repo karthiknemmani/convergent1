e3a9a5c31cc4f7a9ce2f522dd8340ef250f6f732
karthiknemmani <karthiknemmani@gmail.com>
2022-11-23 15:40:16 -0600

    message

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import {
   Alert,
   Button,
@@ -11,12 +11,14 @@ import {
   Touchable,
   TextInput,
 } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+function LoginScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Image
-        style={{ width: "50%", height: "25%" }}
+        style={{ width: "30%", height: "30%" }}
         source={require("./assets/AppLogo.png")}
       />
       <TextInput style={styles.inputText} placeholder="Username" />
@@ -27,38 +29,55 @@ export default function App() {
       </TouchableOpacity>
       <Button title="New? Sign up" color="#E3967E" />
       <Text>or continue with</Text>
-      {/* <SafeAreaView style={styles.iconView}>
+      <View style={styles.iconView}>
         <Image
-          style={{ width: "10%", height: "10%" }}
+          style={{ width: "25%", height: "25%" }}
           source={require("./assets/google.png")}
         />
         <Image
-          style={{ width: "10%", height: "10%" }}
+          style={{ width: "25%", height: "25%" }}
           source={require("./assets/facebook.png")}
         />
-      </SafeAreaView> */}
+      </View>
     </SafeAreaView>
   );
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    inputText: {
-      height: 20,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-    iconView: {
-      flexDirection: "row",
-    },
-    googlefacebook: {
-      width: "20%",
-      height: "20%",
-    },
-  });
 }
+
+function signIn() {
+  return <Text>Sign Up</Text>;
+}
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 200,
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputText: {
+    height: 20,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  iconView: {
+    padding: 20,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "50%",
+    height: "50%",
+  },
+});
